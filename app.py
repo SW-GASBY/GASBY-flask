de06096b7f10c6bc58830cad421eddbb4db98d45
ParkHyunseo <sspp8585@icloud.com>
2024-07-16 21:26:42 +0900

    Fix: json file naming change to uuid

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def get_video():
         bucket_name1 = 'gasby-mot-result'
         object_name = payload + '/'  # Optional
         # s3.upload_file(file_name1 + 'frames.pkl', bucket_name1, object_name + 'frames.pkl')
-        s3.upload_file(file_name1 + 'player_positions_filtered.json', bucket_name1, object_name + 'player_positions_filtered.json')
+        s3.upload_file(file_name1 + 'player_positions_filtered.json', bucket_name1, object_name + payload + '.json')
         # 로컬파일경로 + 파일명 + 파일종류, 버킷명, s3버킷의 원하는경로 + 파일명 + 파일종류
 
         ################################################################ 밑에 주석 풀면 로컬파일 삭제 됨. ################################################################
